620d9b10d595d95212a9fc21827de3923cbe7754
tudornico <46244937+tudornico@users.noreply.github.com>
2021-05-04 10:48:55 +0300

    Update Domain.cpp

--- Domain.cpp
+++ Domain.cpp
@@ -1,1 +1,59 @@
 #include "Domain.h"
+
+using namespace Domain;
+
+Car::Car(const string & model, const string & brand, const string & fuel, const int & yearofregistration,
+         const int & kilometers, const int & price, const int & power) {
+    this->model = model;
+    this->brand = brand;
+    this->fuel = fuel;
+    this->yearOfRegistration = yearofregistration;
+    this->kilometers = kilometers;
+    this->price = price;
+    this->power = power;
+}
+int Car::get_Power() {
+    return this->power;
+}
+
+string Car::get_Model() {
+
+    return this->model;
+}
+
+string Car::get_Brand() {
+    return this->brand;
+}
+
+int Car::get_Kilometers() {
+    return this->kilometers;
+}
+
+int Car::get_Price() {
+    return this->price;
+}
+
+int Car::get_Year() {
+    return this->yearOfRegistration;
+}
+
+string Car::get_Fuel() {
+    return this->fuel;
+}
+
+void Car::set_Kilometers(int _kilometers) {
+
+    this->kilometers = _kilometers;
+}
+
+void Car::set_Price(int _price) {
+    this->price = _price;
+}
+bool Car::checker(Car other) {
+    if(this->price==other.get_Price() && this->kilometers==other.get_Kilometers() && this->fuel==other.get_Fuel() && this->yearOfRegistration==other.get_Year() &&
+    this->brand==other.get_Brand() &&this->power==other.get_Power() && this->model==other.get_Model())
+        return true;
+    return false;
+}
+
+Car::~Car() =default;
