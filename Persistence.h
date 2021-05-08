70e31b8c5bffcda43b2ae4bbbfd589065a56c790
tudornico <46244937+tudornico@users.noreply.github.com>
2021-05-08 21:19:27 +0300

    Update Persistence.h
    rularea pt customer o sa fucntioneze pe baza de username asa ca mereu cand intram in rulare se va cere usernameul ca parametru

--- Persistence.h
+++ Persistence.h
@@ -1,25 +1,37 @@
 #pragma once
-#include "Domain.h"
+#include "Business.h"
 #include <iostream>
-#include <vector>
+#include "Kunde.h"
 using namespace std;
-using namespace Domain;
+using namespace Controller;
 
-namespace Repository
-{
-  class CarRepo
-  {
-  public:
-    CarRepo();
-    void add(const Car &car);
-    bool remove(const Car &car);
-    bool find(const Car& car);
-    void update_Kilometers(Car &car, int _kilometers);    //oare const din add ne deranjeaza in update? (l-am introdus ca si constant, iar noi vrem sa il stergem)
-    void update_Price(Car &car, int _price);              //do we need const here too? - intrebare pentru prof
-    vector<Car> getAll();
-    ~CarRepo();
+namespace UI{
+    class Console
+    {
+    private:
 
-  private:
-    vector<Car> list;
-  };
+
+        //methods of car repo
+        void add(const Car &car);
+        bool remove(const Car &car);
+        bool find(const Car &car);
+        void update_Kilometers(Car &car, int _kilometers);
+        void update_Price(Car &car, int _price);
+        vector<Car> getAll();
+
+        //individual methods of controller
+        vector<Car> findBrand(string brand);
+        vector<Car> findModel(string model);
+        vector<Car> findBrandAndModel(string brand, string model);
+        vector<Car> filterByAge(int age);
+        vector<Car> filterByKilometers(int kilometers);
+        vector<Car> filterByAgeAndKilometers(int age, int kilometers);
+        vector<Car> sortByPrice(int price);
+        Car reader();
+    public:
+        CarController & _ctrl;
+        void manager_run();
+        void customer_run(Client::Kunde client);
+
+    };
 };
