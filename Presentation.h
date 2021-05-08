6d73b35c1ce02e731838edfcdb06ef4c6eef0ea1
tudornico <46244937+tudornico@users.noreply.github.com>
2021-05-08 21:20:11 +0300

    Update Presentation.h
    customer run e pe baza de client pt fiecare client in parte

--- Presentation.h
+++ Presentation.h
@@ -1,35 +1,37 @@
 #pragma once
 #include "Business.h"
 #include <iostream>
+#include "Kunde.h"
 using namespace std;
 using namespace Controller;
 
 namespace UI{
-  class Console
-  {
-  private:
-    CarController & _ctrl;
+    class Console
+    {
+    private:
 
-    //methods of car repo
-    void add(const Car &car);
-    bool remove(const Car &car);
-    bool find(const Car &car);
-    void update_Kilometers(Car &car, int _kilometers); 
-    void update_Price(Car &car, int _price);           
-    vector<Car> getAll();
 
-    //individual methods of controller
-    vector<Car> findBrand(string brand);
-    vector<Car> findModel(string model);
-    vector<Car> findBrandAndModel(string brand, string model);
-    vector<Car> filterByAge(int age);
-    vector<Car> filterByKilometers(int kilometers);
-    vector<Car> filterByAgeAndKilometers(int age, int kilometers);
-    vector<Car> sortByPrice(int price);
+        //methods of car repo
+        void add(const Car &car);
+        bool remove(const Car &car);
+        bool find(const Car &car);
+        void update_Kilometers(Car &car, int _kilometers);
+        void update_Price(Car &car, int _price);
+        vector<Car> getAll();
 
-  public:
-    Console(CarController &);
+        //individual methods of controller
+        vector<Car> findBrand(string brand);
+        vector<Car> findModel(string model);
+        vector<Car> findBrandAndModel(string brand, string model);
+        vector<Car> filterByAge(int age);
+        vector<Car> filterByKilometers(int kilometers);
+        vector<Car> filterByAgeAndKilometers(int age, int kilometers);
+        vector<Car> sortByPrice(int price);
+        Car reader();
+    public:
+        CarController & _ctrl;
+        void manager_run();
+        void customer_run(Client::Kunde client);
 
-    void run();
-  };
+    };
 };
